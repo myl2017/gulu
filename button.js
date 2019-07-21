dee68ed4954b44ee50d460a5f8a65d67a2d046f5
孟艳利 <706695013@qq.com>
2019-07-21 08:10:20 +0800

    安装parcel和node-sass成功

--- button.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by 孟艳丽 on 2019/7/20.
- */
-Vue.component('g-button', {
-    template: `<button class="g-button">按钮</button>`
-})
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,10 @@
 <template>
-    <button class="g-button"></button>
+    <button class="g-button">按钮</button>
 </template>
 <script>
     export default {}
 </script>
-<style lang="scss">
+<style scope lang="scss">
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
