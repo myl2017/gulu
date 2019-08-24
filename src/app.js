e38fcfbe8b5fcfa77b705dab515381234fb5d080
孟艳利 <706695013@qq.com>
2019-08-24 17:09:02 +0800

    网格系统 row and col 组件

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
+
 
 const app = new Vue({
     el: '#app',
@@ -22,7 +27,7 @@ const app = new Vue({
     created(){
         setTimeout(() => {
             let event = new Event('change')
-            let inputElement = vm.$el.querySelector('.input-change')
+            let inputElement = app.$el.querySelector('.input-change')
             inputElement.dispatchEvent(event)
             console.log('hi')
         }, 3000)
---
+++ src/row.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
+        <!--插槽-->
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuluRow',
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        created() {
+            console.log('row created')
+        },
+        mounted() {
+            console.log('row mounted')
+            console.log(this.$children)
+            this.$children.forEach((vm)=> {
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+    }
+</style>
