35cc135e92c2f6d55a68ee98b282ffec4c7c0293
孟艳利 <706695013@qq.com>
2019-09-22 16:55:18 +0800

    实现layout header content footer sider 组件

--- test/row.test.js
+++ test/row.test.js
@@ -11,20 +11,32 @@ describe('Row', () => {
         expect(Row).to.be.ok
     })
 
-    // it('接收 gutter 属性.', () => {
-    //     const div = document.createElement('div')
-    //     document.body.appendChild(div)
-    //     const RowConstructor = Vue.extend(Row)
-    //     const row = new RowConstructor({
-    //         propsData: {
-    //             gutter: 10,
-    //         }
-    //     }).$mount(div)
-    //     const ColConstructor = Vue.extend(Col)
-    //     const col = new ColConstructor()
-    //     col.$mount(row)
-    //     console.log(row.$el.outerHTML)
-    //     expect(1).to.eq(1)
-    //     row.$destroy()
-    // })
+    it('接收 gutter 属性.', (done) => {
+        Vue.component('g-row', Row)
+        Vue.component('g-col', Col)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+            <g-row gutter="200">
+                <g-col span="12"></g-col>
+                <g-col span="12"></g-col>
+            </g-row>
+            `
+        const vm = new Vue({
+            el: div
+        })
+        setTimeout(() => {
+            const row = vm.$el.querySelector('.row')
+            expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+            expect(getComputedStyle(row).marginRight).to.eq('-10px')
+            const cols = vm.$el.querySelectorAll('.col')
+            expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+            expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+            console.log(vm.$el.outerHTML)
+            console.log('如果打出来了会看见这句话')
+            done()
+            vm.$el.remove()
+            vm.$destroy() m
+        }, 1500)
+    })
 })
