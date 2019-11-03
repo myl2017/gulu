477d3d0278845dc8bc652eb57a35976e7d59f799
孟艳利 <706695013@qq.com>
2019-11-03 15:02:09 +0800

    添加popover 组件

---
+++ src/popover.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="popover"  @click="xxx">
+        <div class="content-wrapper">
+            <slot name="content" v-if="visible"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuluPopover',
+        data() {
+            return {
+                visible: false
+            }
+        },
+        methods:{
+            xxx() {
+                this.visible = !this.visible
+                console.log(1)
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .popover {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        .content-wrapper {
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            border: 1px solid red;
+            box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        }
+    }
+</style>
