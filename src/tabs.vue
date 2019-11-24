e2991d5608531c27ef3e8f4789d15f556395b312
孟艳利 <706695013@qq.com>
2019-11-24 20:44:12 +0800

    popover 组件完成

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,17 +31,30 @@
                 eventBus: this.eventBus
             }
         },
+        methods: {
+            checkChildren() {
+                if (this.$children.length === 0) {
+                    console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
+                }
+            },
+            selectTab() {
+                this.$children.forEach((vm) => {
+                    if(vm.$options.name === 'GuluTabsHead'){
+                      vm.$children.forEach((childVm) => {
+                          if(childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected){
+                              this.eventBus.$emit('update:selected', this.selected, childVm)
+                          }
+                      })
+                    }
+                })
+            }
+        },
         mounted() {
 //            this.$emit('update:selected', 'xxx')
             console.log(this.eventBus)
             console.log(this.$children)
-            this.$children.forEach((vm) => {
-                vm.$children.forEach((childVm) => {
-                   if(childVm.$options.name === 'GuluTabsItem' && childVm.nmae === this.selected){
-                     this.eventBus.$emit('update:selected', this.selected,childVm)
-                   }
-               })
-            })
+            this.checkChildren()
+            this.selectTab()
         }
     }
 </script>
